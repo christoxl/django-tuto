5ac67a7a3bfb7988c2ea5d775b2beddd663019a9
christoxl <christoxl@gmail.com>
2015-12-27 17:11:34 -0600

    Detail view and Http404 Error

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -12,7 +12,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("Estas viendo la pregunta %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("Estas viendo los resultados de la pregunta %s." % question_id)
