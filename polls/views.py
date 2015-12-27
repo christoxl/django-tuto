c00cae301e0339c5264c1475e94a6413a869de1b
christoxl <christoxl@gmail.com>
2015-12-27 13:04:58 -0600

    Created views for detial, results and vote

--- polls/views.py
+++ polls/views.py
@@ -6,10 +6,10 @@ def index(request):
     return HttpResponse("Hola mundo. Estas en el index de polls")
 
 def detail(request, question_id):
-    return HttpResponse("Estás viendo la pregunta %s." % question_id)
+    return HttpResponse("Estas viendo la pregunta %s." % question_id)
 
 def results(request, question_id):
-    return HttpResponse("Estás viendo los resultados de la pregunta %s." % question_id)
+    return HttpResponse("Estas viendo los resultados de la pregunta %s." % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("Estás votando en la pregunta %s." % question_id)
+    return HttpResponse("Estas votando en la pregunta %s." % question_id)
