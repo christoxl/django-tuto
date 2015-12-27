94f08ace0ac23a8458039bef8cf22ad29f71f888
christoxl <christoxl@gmail.com>
2015-12-27 13:09:22 -0600

    Created view for index

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,14 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola mundo. Estas en el index de polls")
+    # return HttpResponse("Hola mundo. Estas en el index de polls")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("Estas viendo la pregunta %s." % question_id)
