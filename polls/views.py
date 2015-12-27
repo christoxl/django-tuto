6740e15c0fd4252a1582aa067a328c2141bca52b
christoxl <christoxl@gmail.com>
2015-12-27 13:03:45 -0600

    Created views for detial, results and vote

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hola mundo. Estas en el index de polls")
+
+def detail(request, question_id):
+    return HttpResponse("Estás viendo la pregunta %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("Estás viendo los resultados de la pregunta %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Estás votando en la pregunta %s." % question_id)
