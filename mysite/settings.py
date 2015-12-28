d93ca1c184d69e94b85e770939c06ca51bf37c5e
christoxl <christoxl@gmail.com>
2015-12-28 13:06:30 -0600

    Set SQLlite as testing db

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,10 +84,7 @@ DATABASES = {
         'USER': 'christoxl',
         'PASSWORD': 'admin14',
         'HOST': 'christoxl.mysql.pythonanywhere-services.com',
-        'TEST': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME':  os.path.join(BASE_DIR, 'test.sqlite3'),
-        },
+        'TEST_NAME': 'christoxl$test',
 
     }
 }
