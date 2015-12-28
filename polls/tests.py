418424ea24b019ef1fb74b657ad64697badfc385
christoxl <christoxl@gmail.com>
2015-12-28 13:45:53 -0600

    More tests for was published recently

--- polls/tests.py
+++ polls/tests.py
@@ -16,3 +16,21 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() debe regresar False para preguntas
+        publicadas hace mas de un dia
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_questions(self):
+        """
+        was_published_recently() debe regresar True para preguntas
+        publicadas el mismo dia
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question =  Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
