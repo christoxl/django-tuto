0048eb63ad146bfb177880e105cd73d5569ec6f2
christoxl <christoxl@gmail.com>
2016-01-05 17:32:19 -0600

    Added Choice to Admin Page

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -12,3 +12,4 @@ class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
