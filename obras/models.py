28d6a9b473919e59519b58ab18430cd9eb585a72
christoxl <christoxl@gmail.com>
2016-01-11 16:24:20 -0600

    Primerm modelo de la app Obras

--- obras/models.py
+++ obras/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Municipio(models.Model):
+    nombre = models.CharField(max_length=70)
+
+class Obra(models.Model):
+
