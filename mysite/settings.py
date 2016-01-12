22f6515f880a594a8dbfdebfcd93d19e4d447905
christoxl <christoxl@gmail.com>
2016-01-12 12:49:19 -0600

    Instalando App Obras

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'obras.apps.ObrasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
