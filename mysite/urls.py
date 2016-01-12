1b22f646354eb74297977257796c041d471b52f9
christoxl <christoxl@gmail.com>
2016-01-12 14:19:28 -0600

    Agregado urls.py a Obras

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
+    url(r'^obras/', include('obras.urls')),
     url(r'^admin/', admin.site.urls),
 ]
