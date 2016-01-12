69bec28928898f11a55953155c796f55985ded30
christoxl <christoxl@gmail.com>
2016-01-12 13:25:29 -0600

    Agregaado display de Municipio y Obra en Models

--- obras/models.py
+++ obras/models.py
@@ -21,7 +21,7 @@ class Obra(models.Model):
     beneficiarios_mujeres = models.IntegerField(default=0)
 
     def __str__(self):
-        return.self.descripcion
+        return self.descripcion
 
 
 
