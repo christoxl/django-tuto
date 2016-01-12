35a0b6e406d6c47abe29fdab410ecdefbd7d2002
christoxl <christoxl@gmail.com>
2016-01-12 14:30:27 -0600

    Agregado vistas para Obras

--- obras/admin.py
+++ obras/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Municipio, Obra
 
-admin.site.register(Municipio, Obra)
+admin.site.register(Municipio)
+admin.site.register(Obra)
