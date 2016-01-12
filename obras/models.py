8b35ef04c2a241676204bc6e05a455cb9d882a53
christoxl <christoxl@gmail.com>
2016-01-12 12:46:21 -0600

    Moodelo de la app Obras

--- obras/models.py
+++ obras/models.py
@@ -7,7 +7,13 @@ class Municipio(models.Model):
     nombre = models.CharField(max_length=70)
 
 class Obra(models.Model):
-    municipio = models.ForeignKey)(Municipio, on_delete=models.CASCADE)
-    descripcion = models.CharField(max_length=700)
+    municipio = models.ForeignKey.(Municipio, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=800)
+    inversion_ejercida = models.DecimalField(max_digits=20, decimal_places=2)
+    avance = models.DecimalField(max_digits=5, decimal_places=2)
+    beneficiarios_hombres = models.IntegerField(default=0)
+    beneficiarios_mujeres = models.IntegerField(default=0)
+
+
 
 
