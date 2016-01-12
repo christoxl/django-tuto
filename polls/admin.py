c87e4d03bf00084b94f5d40f5cba10b439a1cc79
christoxl <christoxl@gmail.com>
2016-01-12 13:14:32 -0600

    Removing Polls from Admin Site

--- polls/admin.py
+++ polls/admin.py
@@ -1,19 +1,19 @@
-from django.contrib import admin
+# from django.contrib import admin
 
 # Register your models here.
-from .models import Choice, Question
+# from .models import Choice, Question
 
 
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
+# class ChoiceInline(admin.TabularInline):
+#    model = Choice
+#    extra = 3
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
+# class QuestionAdmin(admin.ModelAdmin):
+#    fieldsets = [
+#        (None,               {'fields': ['question_text']}),
+#        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#    ]
+#    inlines = [ChoiceInline]
 
 # admin.site.register(Question, QuestionAdmin)
