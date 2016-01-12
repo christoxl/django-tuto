9ea5280bad251d134de7f56977ff2ef36387a1de
christoxl <christoxl@gmail.com>
2016-01-12 14:42:48 -0600

    Added order para municipios en obras

--- obras/admin.py
+++ obras/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Municipio, Obra
 
+
+class ObraAdmin(admin.ModelAdmin):
+    ordering = ('municipios',)
+
 admin.site.register(Municipio)
-admin.site.register(Obra)
+admin.site.register(Obra, ObraAdmin)
