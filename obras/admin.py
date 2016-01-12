f6d9456f346afb1beb2e47b8ec1b47a081bf52be
christoxl <christoxl@gmail.com>
2016-01-12 13:34:06 -0600

    Agregado display de Municipio y Obra en Models

--- obras/admin.py
+++ obras/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Municipio
+from .models import Municipio, Obra
 
 admin.site.register(Municipio)
+admin.site.register(Obra)
