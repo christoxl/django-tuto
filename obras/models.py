0c9767f0046064f3ef2bdd71582af8ad244f1b5c
christoxl <christoxl@gmail.com>
2016-01-19 17:50:54 -0600

    Agregado region a Municipios

--- obras/models.py
+++ obras/models.py
@@ -3,10 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
+
 # Create your models here.
 @python_2_unicode_compatible
 class Municipio(models.Model):
     nombre = models.CharField(max_length=70)
+    region = models.CharField(max_length=300)
 
     class Meta:
         ordering = ['nombre']
@@ -14,10 +16,12 @@ class Municipio(models.Model):
     def __str__(self):
         return self.nombre
 
+
 @python_2_unicode_compatible
 class Obra(models.Model):
     municipio = models.ForeignKey(Municipio, on_delete=models.CASCADE)
     descripcion = models.TextField()
+    dependencia = models.CharField(max_length=100)
     inversion_ejercida = models.DecimalField(max_digits=20, decimal_places=2)
     avance = models.DecimalField(max_digits=5, decimal_places=2)
     beneficiarios_hombres = models.IntegerField(default=0)
