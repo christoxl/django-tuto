149a3806f79fb12be7b53e5bfd9524a943056fb1
christoxl <christoxl@gmail.com>
2016-01-20 14:20:16 -0600

    Agregado modelo de dependencia

--- obras/admin.py
+++ obras/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Municipio, Obra
+from .models import Municipio, Dependencia, Obra
 
 admin.site.register(Municipio)
+admin.site.register(Dependencia)
 admin.site.register(Obra)
