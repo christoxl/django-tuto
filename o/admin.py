6367b8e1a4c58f51ad4dc8b31f64d324756803db
christoxl <christoxl@gmail.com>
2016-01-20 18:35:54 -0600

    Comenzamos con o models

--- o/admin.py
+++ o/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import Municipio, Dependencia, Obra
 
-admin.site.register(Municipio, Dependencia, Obra)
+admin.site.register(Municipio)
