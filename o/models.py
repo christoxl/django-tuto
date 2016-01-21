ac891ce2f618a0af01eb9dba35093a94d9a62f34
christoxl <christoxl@gmail.com>
2016-01-20 18:29:42 -0600

    Comenzamos con o models

--- o/models.py
+++ o/models.py
@@ -1,5 +1,45 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
 
 # Create your models here.
+@python_2_unicode_compatible
+class Municipio(models.Model):
+    nombre = models.CharField(max_length=70)
+    region = models.CharField(max_length=300, default='')
+
+    class Meta:
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
+
+
+@python_2_unicode_compatible
+class Dependencia(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+@python_2_unicode_compatible
+class Obra(models.Model):
+    ESTATUS_CHOICES = (
+        ('En Ejecución', 'En Ejecución'),
+        ('Concluída', 'Concluída'),
+    )
+    municipio = models.ForeignKey(Municipio, on_delete=models.CASCADE)
+    dependencia = models.ForeignKey(Dependencia, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    inversion_ejercida = models.DecimalField(max_digits=20, decimal_places=2)
+    avance = models.DecimalField(max_digits=5, decimal_places=2)
+    estatus = models.CharField(max_length=50, null=True, blank=True, choices=ESTATUS_CHOICES)
+    beneficiarios_hombres = models.IntegerField(default=0)
+    beneficiarios_mujeres = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.descripcion
--- obras/admin.py
+++ obras/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Municipio, Dependencia, Obra
-
-admin.site.register(Municipio)
-admin.site.register(Dependencia)
-admin.site.register(Obra)
